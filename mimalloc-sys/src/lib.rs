e6b8130a57d09514312af65f77eab3198c526f0d
Max Beck <rytonemail@gmail.com>
2019-06-22 15:12:45 -0500

    Add aligned allocation functions to mimalloc-sys
    Also removes a couple ones that are not used in mimallocator, but those
    could be added back.

--- mimalloc-sys/src/lib.rs
+++ mimalloc-sys/src/lib.rs
@@ -4,9 +4,13 @@ use libc::{c_void, size_t};
 
 extern "C" {
     pub fn mi_malloc(size: size_t) -> *mut c_void;
-    pub fn mi_calloc(count: size_t, size: size_t) -> *mut c_void;
-    pub fn mi_realloc(p: *mut c_void, newsize: size_t) -> *mut c_void;
-    pub fn mi_expand(p: *mut c_void, newsize: size_t) -> *mut c_void;
-    pub fn mi_free(p: *mut c_void);
     pub fn mi_malloc_aligned(size: size_t, alignment: size_t) -> *mut c_void;
+
+    pub fn mi_zalloc(size: size_t) -> *mut c_void;
+    pub fn mi_zalloc_aligned(size: size_t, alignment: size_t) -> *mut c_void;
+
+    pub fn mi_realloc(p: *mut c_void, newsize: size_t) -> *mut c_void;
+    pub fn mi_realloc_aligned(p: *mut c_void, newsize: size_t, alignment: size_t) -> *mut c_void;
+
+    pub fn mi_free(p: *mut c_void);
 }
