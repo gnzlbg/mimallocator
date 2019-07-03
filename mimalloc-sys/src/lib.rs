92d29e06aa2c41b4d2a64a0597c091182e3dc9a8
gnzlbg <gonzalobg88@gmail.com>
2019-07-03 22:28:31 +0200

    Implement malloc override

--- mimalloc-sys/src/lib.rs
+++ mimalloc-sys/src/lib.rs
@@ -1,22 +1,38 @@
 //! Raw FFI wrapper over the mimalloc memory allocator
 #![no_std]
-use libc::{c_void, size_t};
+use libc::{c_void, size_t, FILE};
 
 extern "C" {
+    // Standard malloc interface
+
     pub fn mi_malloc(size: size_t) -> *mut c_void;
-    pub fn mi_malloc_aligned(size: size_t, alignment: size_t) -> *mut c_void;
+    pub fn mi_calloc(count: size_t, size: size_t) -> *mut c_void;
+    pub fn mi_realloc(p: *mut c_void, newsize: size_t) -> *mut c_void;
+    pub fn mi_expand(p: *mut c_void, newsize: size_t) -> *mut c_void;
+    pub fn mi_free(p: *mut c_void);
+
+    // Extended functionality
 
     pub fn mi_zalloc(size: size_t) -> *mut c_void;
-    pub fn mi_zalloc_aligned(size: size_t, alignment: size_t) -> *mut c_void;
+    pub fn mi_usable_size(p: *mut c_void) -> size_t;
+    pub fn mi_good_size(size: size_t) -> size_t;
 
-    pub fn mi_realloc(p: *mut c_void, newsize: size_t) -> *mut c_void;
+    pub fn mi_collect(force: bool);
+    pub fn mi_stats_print(out: *mut FILE);
+    pub fn mi_stats_reset();
+
+    // Aligned allocation
+
+    pub fn mi_malloc_aligned(size: size_t, alignment: size_t) -> *mut c_void;
+    pub fn mi_zalloc_aligned(size: size_t, alignment: size_t) -> *mut c_void;
+    pub fn mi_calloc_aligned(
+        count: size_t,
+        size: size_t,
+        alignment: size_t,
+    ) -> *mut c_void;
     pub fn mi_realloc_aligned(
         p: *mut c_void,
         newsize: size_t,
         alignment: size_t,
     ) -> *mut c_void;
-
-    pub fn mi_free(p: *mut c_void);
-
-    pub fn mi_usable_size(p: *mut c_void) -> size_t;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 #![no_std]
 use core::alloc::{GlobalAlloc, Layout};
 
-// Copied from https://github.com/rust-lang/rust/blob/master/src/libstd/sys_common/alloc.rs
 #[cfg(all(any(
     target_arch = "x86",
     target_arch = "arm",
@@ -11,7 +10,7 @@ use core::alloc::{GlobalAlloc, Layout};
     target_arch = "asmjs",
     target_arch = "wasm32"
 )))]
-const MIN_ALIGN: usize = 8;
+const max_align_t: usize = 8;
 #[cfg(all(any(
     target_arch = "x86_64",
     target_arch = "aarch64",
@@ -19,7 +18,7 @@ const MIN_ALIGN: usize = 8;
     target_arch = "s390x",
     target_arch = "sparc64"
 )))]
-const MIN_ALIGN: usize = 16;
+const max_align_t: usize = 16;
 
 #[derive(Copy, Clone, Default, Debug)]
 pub struct Mimalloc;
@@ -27,7 +26,7 @@ pub struct Mimalloc;
 unsafe impl GlobalAlloc for Mimalloc {
     #[inline]
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        let ptr = if layout.align() <= MIN_ALIGN
+        let ptr = if layout.align() <= max_align_t
             && layout.align() <= layout.size()
         {
             mimalloc_sys::mi_malloc(layout.size() as _)
@@ -43,7 +42,7 @@ unsafe impl GlobalAlloc for Mimalloc {
 
     #[inline]
     unsafe fn alloc_zeroed(&self, layout: Layout) -> *mut u8 {
-        let ptr = if layout.align() <= MIN_ALIGN
+        let ptr = if layout.align() <= max_align_t
             && layout.align() <= layout.size()
         {
             mimalloc_sys::mi_zalloc(layout.size() as _)
@@ -69,7 +68,7 @@ unsafe impl GlobalAlloc for Mimalloc {
         layout: Layout,
         new_size: usize,
     ) -> *mut u8 {
-        let ptr = if layout.align() <= MIN_ALIGN
+        let ptr = if layout.align() <= max_align_t
             && layout.align() <= layout.size()
         {
             mimalloc_sys::mi_realloc(ptr as *mut _, new_size)
