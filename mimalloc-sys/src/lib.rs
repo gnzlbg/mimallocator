f73d7addc7c78c9e9cf3660f21a325e9d6b382cf
gnzlbg <gonzalobg88@gmail.com>
2019-09-12 17:57:50 +0200

    Upgrade to dev branch

--- mimalloc-sys/src/lib.rs
+++ mimalloc-sys/src/lib.rs
@@ -1,6 +1,6 @@
 //! Raw FFI wrapper over the mimalloc memory allocator
 #![no_std]
-use libc::{c_void, size_t, FILE, c_int};
+use libc::{c_void, size_t, c_char, c_int};
 
 extern "C" {
     // Standard malloc interface
@@ -22,7 +22,7 @@ extern "C" {
     pub fn mi_good_size(size: size_t) -> size_t;
 
     pub fn mi_collect(force: bool);
-    pub fn mi_stats_print(out: *mut FILE);
+    pub fn mi_stats_print(out: Option<unsafe extern "C" fn(*const c_char)>);
     pub fn mi_stats_reset();
 
     // Aligned allocation
@@ -50,7 +50,7 @@ extern "C" {
     pub fn free(p: *mut c_void);
     pub fn posix_memalign(ptr: *mut *mut c_void, alignment: size_t, size: size_t) -> c_int;
     pub fn aligned_alloc(alignment: size_t, size: size_t) -> *mut c_void;
-    pub fn malloc_size(p: *mut c_void) -> size_t;
-    pub fn malloc_usable_size(p: *mut c_void) -> size_t;
+    pub fn malloc_size(p: *const c_void) -> size_t;
+    pub fn malloc_usable_size(p: *const c_void) -> size_t;
 
 }
